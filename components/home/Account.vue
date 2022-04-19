980c75e67d5579449e6180b2562cfab925bcde18
daniel.meneses <daniel.meneses@cuemby.com>
2022-04-19 18:07:21 -0500

    add home section 2

---
+++ components/home/Account.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="account-container">
+    <div>
+      <v-avatar
+        rounded
+        size="45"
+      >
+        <img :src="account.img" alt="">
+      </v-avatar>
+    </div>
+    <div class="account-text">
+      <span class="text-h6">{{ account.title }}</span>
+      <span class="text-caption">{{ account.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Account',
+    props: {
+      account: {
+        type: Object,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style>
+  .account-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px;
+  }
+  .account-text {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+  }
+</style>
---
+++ components/home/Section2.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-row>
+        <v-col
+          v-for="account in accounts"
+          :key="account.title"
+        >
+          <Account
+            :account="account"
+          />
+        </v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import Account from './Account.vue'
+  export default {
+    name: 'Section2',
+    components: { Account },
+    data() {
+        return {
+            accounts: [
+                {
+                    img: '/home/section2/BNB.png',
+                    title: '534',
+                    text: 'Biance accounts',
+                },
+                {
+                    img: '/home/section2/USDT.png',
+                    title: '$122,673,328 USDT',
+                    text: 'Monthly purchasing power',
+                }
+            ]
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
